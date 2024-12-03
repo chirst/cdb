4a7a6359eb216563c207abc2251b83d58476c210
chirst <colton141@live.com>
2024-12-03 00:50:22 -0700

    table tests for select planner

--- planner/select_test.go
+++ planner/select_test.go
@@ -32,262 +32,240 @@ func (m *mockSelectCatalog) GetPrimaryKeyColumn(tableName string) (string, error
 	return m.primaryKeyColumnName, nil
 }
 
-func TestGetPlan(t *testing.T) {
-	expectedCommands := []vm.Command{
-		&vm.InitCmd{P2: 1},
-		&vm.TransactionCmd{P1: 0},
-		&vm.OpenReadCmd{P1: 1, P2: 2},
-		&vm.RewindCmd{P1: 1, P2: 9},
-		&vm.ColumnCmd{P1: 1, P2: 0, P3: 1},
-		&vm.RowIdCmd{P1: 1, P2: 2},
-		&vm.ColumnCmd{P1: 1, P2: 1, P3: 3},
-		&vm.ResultRowCmd{P1: 1, P2: 3},
-		&vm.NextCmd{P1: 1, P2: 4},
-		&vm.HaltCmd{},
+func TestSelectPlan(t *testing.T) {
+	type selectCase struct {
+		description      string
+		ast              *compiler.SelectStmt
+		expectedCommands []vm.Command
+		mockCatalogSetup func(m *mockSelectCatalog) *mockSelectCatalog
 	}
-	ast := &compiler.SelectStmt{
-		StmtBase: &compiler.StmtBase{},
-		From: &compiler.From{
-			TableName: "foo",
-		},
-		ResultColumns: []compiler.ResultColumn{
-			{
-				All: true,
+	cases := []selectCase{
+		{
+			description: "StarWithPrimaryKey",
+			expectedCommands: []vm.Command{
+				&vm.InitCmd{P2: 1},
+				&vm.TransactionCmd{P1: 0},
+				&vm.OpenReadCmd{P1: 1, P2: 2},
+				&vm.RewindCmd{P1: 1, P2: 8},
+				&vm.RowIdCmd{P1: 1, P2: 1},
+				&vm.ColumnCmd{P1: 1, P2: 0, P3: 2},
+				&vm.ResultRowCmd{P1: 1, P2: 2},
+				&vm.NextCmd{P1: 1, P2: 4},
+				&vm.HaltCmd{},
+			},
+			ast: &compiler.SelectStmt{
+				StmtBase: &compiler.StmtBase{},
+				From: &compiler.From{
+					TableName: "foo",
+				},
+				ResultColumns: []compiler.ResultColumn{
+					{
+						All: true,
+					},
+				},
+			},
+			mockCatalogSetup: func(m *mockSelectCatalog) *mockSelectCatalog {
+				m.primaryKeyColumnName = "id"
+				return m
 			},
 		},
-	}
-	mockCatalog := &mockSelectCatalog{}
-	mockCatalog.primaryKeyColumnName = "id"
-	mockCatalog.columns = []string{"name", "id", "age"}
-	plan, err := NewSelect(mockCatalog, ast).ExecutionPlan()
-	if err != nil {
-		t.Errorf("expected no err got err %s", err)
-	}
-	for i, c := range expectedCommands {
-		if !reflect.DeepEqual(c, plan.Commands[i]) {
-			t.Errorf("got %#v want %#v", plan.Commands[i], c)
-		}
-	}
-}
-
-func TestGetPlanSelectColumn(t *testing.T) {
-	expectedCommands := []vm.Command{
-		&vm.InitCmd{P2: 1},
-		&vm.TransactionCmd{P1: 0},
-		&vm.OpenReadCmd{P1: 1, P2: 2},
-		&vm.RewindCmd{P1: 1, P2: 7},
-		&vm.RowIdCmd{P1: 1, P2: 1},
-		&vm.ResultRowCmd{P1: 1, P2: 1},
-		&vm.NextCmd{P1: 1, P2: 4},
-		&vm.HaltCmd{},
-	}
-	ast := &compiler.SelectStmt{
-		StmtBase: &compiler.StmtBase{},
-		From: &compiler.From{
-			TableName: "foo",
+		{
+			description: "StarWithoutPrimaryKey",
+			expectedCommands: []vm.Command{
+				&vm.InitCmd{P2: 1},
+				&vm.TransactionCmd{P1: 0},
+				&vm.OpenReadCmd{P1: 1, P2: 2},
+				&vm.RewindCmd{P1: 1, P2: 8},
+				&vm.ColumnCmd{P1: 1, P2: 0, P3: 1},
+				&vm.ColumnCmd{P1: 1, P2: 1, P3: 2},
+				&vm.ResultRowCmd{P1: 1, P2: 2},
+				&vm.NextCmd{P1: 1, P2: 4},
+				&vm.HaltCmd{},
+			},
+			ast: &compiler.SelectStmt{
+				StmtBase: &compiler.StmtBase{},
+				From: &compiler.From{
+					TableName: "foo",
+				},
+				ResultColumns: []compiler.ResultColumn{
+					{
+						All: true,
+					},
+				},
+			},
 		},
-		ResultColumns: []compiler.ResultColumn{
-			{
-				Expression: &compiler.ColumnRef{
-					Column: "id",
+		{
+			description: "StarPrimaryKeyMiddleOrdinal",
+			ast: &compiler.SelectStmt{
+				StmtBase: &compiler.StmtBase{},
+				From: &compiler.From{
+					TableName: "foo",
+				},
+				ResultColumns: []compiler.ResultColumn{
+					{
+						All: true,
+					},
+				},
+			},
+			expectedCommands: []vm.Command{
+				&vm.InitCmd{P2: 1},
+				&vm.TransactionCmd{P1: 0},
+				&vm.OpenReadCmd{P1: 1, P2: 2},
+				&vm.RewindCmd{P1: 1, P2: 9},
+				&vm.ColumnCmd{P1: 1, P2: 0, P3: 1},
+				&vm.RowIdCmd{P1: 1, P2: 2},
+				&vm.ColumnCmd{P1: 1, P2: 1, P3: 3},
+				&vm.ResultRowCmd{P1: 1, P2: 3},
+				&vm.NextCmd{P1: 1, P2: 4},
+				&vm.HaltCmd{},
+			},
+			mockCatalogSetup: func(m *mockSelectCatalog) *mockSelectCatalog {
+				m.primaryKeyColumnName = "id"
+				m.columns = []string{"name", "id", "age"}
+				return m
+			},
+		},
+		{
+			description: "AllTable",
+			expectedCommands: []vm.Command{
+				&vm.InitCmd{P2: 1},
+				&vm.TransactionCmd{P1: 0},
+				&vm.OpenReadCmd{P1: 1, P2: 2},
+				&vm.RewindCmd{P1: 1, P2: 8},
+				&vm.RowIdCmd{P1: 1, P2: 1},
+				&vm.ColumnCmd{P1: 1, P2: 0, P3: 2},
+				&vm.ResultRowCmd{P1: 1, P2: 2},
+				&vm.NextCmd{P1: 1, P2: 4},
+				&vm.HaltCmd{},
+			},
+			ast: &compiler.SelectStmt{
+				StmtBase: &compiler.StmtBase{},
+				From: &compiler.From{
+					TableName: "foo",
+				},
+				ResultColumns: []compiler.ResultColumn{
+					{
+						AllTable: "foo",
+					},
+				},
+			},
+			mockCatalogSetup: func(m *mockSelectCatalog) *mockSelectCatalog {
+				m.primaryKeyColumnName = "id"
+				m.columns = []string{"id", "name"}
+				return m
+			},
+		},
+		{
+			description: "SpecificColumnPrimaryKeyMiddleOrdinal",
+			expectedCommands: []vm.Command{
+				&vm.InitCmd{P2: 1},
+				&vm.TransactionCmd{P1: 0},
+				&vm.OpenReadCmd{P1: 1, P2: 2},
+				&vm.RewindCmd{P1: 1, P2: 7},
+				&vm.RowIdCmd{P1: 1, P2: 1},
+				&vm.ResultRowCmd{P1: 1, P2: 1},
+				&vm.NextCmd{P1: 1, P2: 4},
+				&vm.HaltCmd{},
+			},
+			ast: &compiler.SelectStmt{
+				StmtBase: &compiler.StmtBase{},
+				From: &compiler.From{
+					TableName: "foo",
+				},
+				ResultColumns: []compiler.ResultColumn{
+					{
+						Expression: &compiler.ColumnRef{
+							Column: "id",
+						},
+					},
+				},
+			},
+			mockCatalogSetup: func(m *mockSelectCatalog) *mockSelectCatalog {
+				m.primaryKeyColumnName = "id"
+				m.columns = []string{"name", "id", "age"}
+				return m
+			},
+		},
+		{
+			description: "SpecificColumns",
+			expectedCommands: []vm.Command{
+				&vm.InitCmd{P2: 1},
+				&vm.TransactionCmd{P1: 0},
+				&vm.OpenReadCmd{P1: 1, P2: 2},
+				&vm.RewindCmd{P1: 1, P2: 8},
+				&vm.RowIdCmd{P1: 1, P2: 1},
+				&vm.ColumnCmd{P1: 1, P2: 1, P3: 2},
+				&vm.ResultRowCmd{P1: 1, P2: 2},
+				&vm.NextCmd{P1: 1, P2: 4},
+				&vm.HaltCmd{},
+			},
+			ast: &compiler.SelectStmt{
+				StmtBase: &compiler.StmtBase{},
+				From: &compiler.From{
+					TableName: "foo",
+				},
+				ResultColumns: []compiler.ResultColumn{
+					{
+						Expression: &compiler.ColumnRef{
+							Column: "id",
+						},
+					},
+					{
+						Expression: &compiler.ColumnRef{
+							Column: "age",
+						},
+					},
+				},
+			},
+			mockCatalogSetup: func(m *mockSelectCatalog) *mockSelectCatalog {
+				m.primaryKeyColumnName = "id"
+				m.columns = []string{"name", "id", "age"}
+				return m
+			},
+		},
+		{
+			description: "JustCountAggregate",
+			expectedCommands: []vm.Command{
+				&vm.InitCmd{P2: 1},
+				&vm.TransactionCmd{P1: 0},
+				&vm.OpenReadCmd{P1: 1, P2: 2},
+				&vm.CountCmd{P1: 1, P2: 1},
+				&vm.ResultRowCmd{P1: 1, P2: 1},
+				&vm.HaltCmd{},
+			},
+			ast: &compiler.SelectStmt{
+				StmtBase: &compiler.StmtBase{},
+				From: &compiler.From{
+					TableName: "foo",
+				},
+				ResultColumns: []compiler.ResultColumn{
+					{
+						Expression: &compiler.FunctionExpr{FnType: compiler.FnCount},
+					},
 				},
 			},
 		},
 	}
-	mockCatalog := &mockSelectCatalog{}
-	mockCatalog.primaryKeyColumnName = "id"
-	mockCatalog.columns = []string{"name", "id", "age"}
-	plan, err := NewSelect(mockCatalog, ast).ExecutionPlan()
-	if err != nil {
-		t.Errorf("expected no err got err %s", err)
-	}
-	for i, c := range expectedCommands {
-		if !reflect.DeepEqual(c, plan.Commands[i]) {
-			t.Errorf("got %#v want %#v", plan.Commands[i], c)
+	for _, c := range cases {
+		if c.description == "" {
+			t.Fatal("cases must have description")
 		}
-	}
-}
-
-func TestGetPlanSelectMultiColumn(t *testing.T) {
-	expectedCommands := []vm.Command{
-		&vm.InitCmd{P2: 1},
-		&vm.TransactionCmd{P1: 0},
-		&vm.OpenReadCmd{P1: 1, P2: 2},
-		&vm.RewindCmd{P1: 1, P2: 8},
-		&vm.RowIdCmd{P1: 1, P2: 1},
-		&vm.ColumnCmd{P1: 1, P2: 1, P3: 2},
-		&vm.ResultRowCmd{P1: 1, P2: 2},
-		&vm.NextCmd{P1: 1, P2: 4},
-		&vm.HaltCmd{},
-	}
-	ast := &compiler.SelectStmt{
-		StmtBase: &compiler.StmtBase{},
-		From: &compiler.From{
-			TableName: "foo",
-		},
-		ResultColumns: []compiler.ResultColumn{
-			{
-				Expression: &compiler.ColumnRef{
-					Column: "id",
-				},
-			},
-			{
-				Expression: &compiler.ColumnRef{
-					Column: "age",
-				},
-			},
-		},
-	}
-	mockCatalog := &mockSelectCatalog{}
-	mockCatalog.primaryKeyColumnName = "id"
-	mockCatalog.columns = []string{"name", "id", "age"}
-	plan, err := NewSelect(mockCatalog, ast).ExecutionPlan()
-	if err != nil {
-		t.Errorf("expected no err got err %s", err)
-	}
-	for i, c := range expectedCommands {
-		if !reflect.DeepEqual(c, plan.Commands[i]) {
-			t.Errorf("got %#v want %#v", plan.Commands[i], c)
-		}
-	}
-}
-
-func TestGetPlanTableAll(t *testing.T) {
-	expectedCommands := []vm.Command{
-		&vm.InitCmd{P2: 1},
-		&vm.TransactionCmd{P1: 0},
-		&vm.OpenReadCmd{P1: 1, P2: 2},
-		&vm.RewindCmd{P1: 1, P2: 8},
-		&vm.RowIdCmd{P1: 1, P2: 1},
-		&vm.ColumnCmd{P1: 1, P2: 0, P3: 2},
-		&vm.ResultRowCmd{P1: 1, P2: 2},
-		&vm.NextCmd{P1: 1, P2: 4},
-		&vm.HaltCmd{},
-	}
-	ast := &compiler.SelectStmt{
-		StmtBase: &compiler.StmtBase{},
-		From: &compiler.From{
-			TableName: "foo",
-		},
-		ResultColumns: []compiler.ResultColumn{
-			{
-				AllTable: "foo",
-			},
-		},
-	}
-	mockCatalog := &mockSelectCatalog{}
-	mockCatalog.primaryKeyColumnName = "id"
-	mockCatalog.columns = []string{"id", "name"}
-	plan, err := NewSelect(mockCatalog, ast).ExecutionPlan()
-	if err != nil {
-		t.Errorf("expected no err got err %s", err)
-	}
-	for i, c := range expectedCommands {
-		if !reflect.DeepEqual(c, plan.Commands[i]) {
-			t.Errorf("got %#v want %#v", plan.Commands[i], c)
-		}
-	}
-}
-
-func TestGetPlanPKMiddleOrdinal(t *testing.T) {
-	expectedCommands := []vm.Command{
-		&vm.InitCmd{P2: 1},
-		&vm.TransactionCmd{P1: 0},
-		&vm.OpenReadCmd{P1: 1, P2: 2},
-		&vm.RewindCmd{P1: 1, P2: 8},
-		&vm.RowIdCmd{P1: 1, P2: 1},
-		&vm.ColumnCmd{P1: 1, P2: 0, P3: 2},
-		&vm.ResultRowCmd{P1: 1, P2: 2},
-		&vm.NextCmd{P1: 1, P2: 4},
-		&vm.HaltCmd{},
-	}
-	ast := &compiler.SelectStmt{
-		StmtBase: &compiler.StmtBase{},
-		From: &compiler.From{
-			TableName: "foo",
-		},
-		ResultColumns: []compiler.ResultColumn{
-			{
-				All: true,
-			},
-		},
-	}
-	mockCatalog := &mockSelectCatalog{}
-	mockCatalog.primaryKeyColumnName = "id"
-	plan, err := NewSelect(mockCatalog, ast).ExecutionPlan()
-	if err != nil {
-		t.Errorf("expected no err got err %s", err)
-	}
-	for i, c := range expectedCommands {
-		if !reflect.DeepEqual(c, plan.Commands[i]) {
-			t.Errorf("got %#v want %#v", plan.Commands[i], c)
-		}
-	}
-}
-
-func TestGetCountAggregate(t *testing.T) {
-	expectedCommands := []vm.Command{
-		&vm.InitCmd{P2: 1},
-		&vm.TransactionCmd{P1: 0},
-		&vm.OpenReadCmd{P1: 1, P2: 2},
-		&vm.CountCmd{P1: 1, P2: 1},
-		&vm.ResultRowCmd{P1: 1, P2: 1},
-		&vm.HaltCmd{},
-	}
-	ast := &compiler.SelectStmt{
-		StmtBase: &compiler.StmtBase{},
-		From: &compiler.From{
-			TableName: "foo",
-		},
-		ResultColumns: []compiler.ResultColumn{
-			{
-				Expression: &compiler.FunctionExpr{FnType: compiler.FnCount},
-			},
-		},
-	}
-	mockCatalog := &mockSelectCatalog{}
-	plan, err := NewSelect(mockCatalog, ast).ExecutionPlan()
-	if err != nil {
-		t.Errorf("expected no err got err %s", err)
-	}
-	for i, c := range expectedCommands {
-		if !reflect.DeepEqual(c, plan.Commands[i]) {
-			t.Errorf("got %#v want %#v", plan.Commands[i], c)
-		}
-	}
-}
-
-func TestGetPlanNoPrimaryKey(t *testing.T) {
-	expectedCommands := []vm.Command{
-		&vm.InitCmd{P2: 1},
-		&vm.TransactionCmd{P1: 0},
-		&vm.OpenReadCmd{P1: 1, P2: 2},
-		&vm.RewindCmd{P1: 1, P2: 8},
-		&vm.ColumnCmd{P1: 1, P2: 0, P3: 1},
-		&vm.ColumnCmd{P1: 1, P2: 1, P3: 2},
-		&vm.ResultRowCmd{P1: 1, P2: 2},
-		&vm.NextCmd{P1: 1, P2: 4},
-		&vm.HaltCmd{},
-	}
-	ast := &compiler.SelectStmt{
-		StmtBase: &compiler.StmtBase{},
-		From: &compiler.From{
-			TableName: "foo",
-		},
-		ResultColumns: []compiler.ResultColumn{
-			{
-				All: true,
-			},
-		},
-	}
-	mockCatalog := &mockSelectCatalog{}
-	plan, err := NewSelect(mockCatalog, ast).ExecutionPlan()
-	if err != nil {
-		t.Errorf("expected no err got err %s", err)
-	}
-	for i, c := range expectedCommands {
-		if !reflect.DeepEqual(c, plan.Commands[i]) {
-			t.Errorf("got %#v want %#v", plan.Commands[i], c)
-		}
+		t.Run(c.description, func(t *testing.T) {
+			if c.mockCatalogSetup == nil {
+				c.mockCatalogSetup = func(m *mockSelectCatalog) *mockSelectCatalog {
+					return m
+				}
+			}
+			mockCatalog := c.mockCatalogSetup(&mockSelectCatalog{})
+			plan, err := NewSelect(mockCatalog, c.ast).ExecutionPlan()
+			if err != nil {
+				t.Errorf("expected no err got err %s", err)
+			}
+			for i, c := range c.expectedCommands {
+				if !reflect.DeepEqual(c, plan.Commands[i]) {
+					t.Errorf("got %#v want %#v", plan.Commands[i], c)
+				}
+			}
+		})
 	}
 }
